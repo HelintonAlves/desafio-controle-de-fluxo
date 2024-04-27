85ba767f9225f788b14c3686319211f5b1367ae8
HelintonAlves <helinton-alves@hotmail.com>
2024-04-27 19:40:47 -0300

    Primeiro commit do código proposto pelo desafio controle de fluxo

---
+++ src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro (um número inteiro): " );
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Ditite o segundo parâmetro (número inteiro): ");
+        int parametroDois = terminal.nextInt();
+
+        try{
+            contar(parametroUm, parametroDois);
+        }catch (ParametrosInvalidosException mensagem){
+            System.out.println(mensagem);
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroDois < parametroUm){
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for(int contador = 0; contador < contagem; contador++){
+            System.out.println("Número: " + (contador + 1));
+        }
+
+    }
+
+}
---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+public class ParametrosInvalidosException extends Throwable {
+
+    public ParametrosInvalidosException(){
+        super("parametroDois é menor que o parametroUm!");
+    }
+
+}
